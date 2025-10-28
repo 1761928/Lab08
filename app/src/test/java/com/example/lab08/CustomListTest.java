503e2414a1a1b0d7990c12e841f6a91664251fb3
Jack Rappel <jrappel@ualberta.ca>
2025-10-28 17:26:45 -0600

    Testing hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    private CustomList mockCityList() {
+        CustomList cityList = new CustomList();
+        cityList.addCity(mockCity());
+        return cityList;
+    }
+
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    void testHasCity() {
+        CustomList cityList = mockCityList();
+        City city = new City("Regina", "Saskatchewan");
+        cityList.addCity(city);
+        assertTrue(cityList.hasCity(city));
+        City city2 = new City("Charlottetown", "Prince Edward Island");
+        assertFalse(cityList.hasCity(city2));
+    }
+
 }
